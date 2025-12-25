e23b8c4c585c6e09a223f98e98e0183ce0c42568
phamphuvi <ppvi2101609@student.ctuet.edu.vn>
2025-12-25 12:44:30 +0700

    Fix formatting in heroTitle for Vietnamese text

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const I18N = {
     vi: {
       // Dòng này đã được làm đẹp với thẻ <br> và <span highlight>
-      heroTitle: "Xin chào, mình là <span class='text-highlight'>Vĩ</span>. Xin mời bạn tham quan<br> dự án Cá nhân của mình.<br>",
+      heroTitle: "Xin chào, mình là <span class='text-highlight'>Vĩ.</span>Xin mời bạn tham quan<br> dự án Cá nhân của mình.<br>",
       
       roleText: "Sinh viên Kỹ thuật & IoT",
       navAbout: "Về tôi", navProjects: "Dự án", navSkills: "Kỹ năng", navContact: "Liên hệ",
